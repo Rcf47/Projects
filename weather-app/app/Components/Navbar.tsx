e5a62546f0eb42e3bd1c3ca1791e0566eae07b9e
Vadim Cherepanov <xenos26@gmail.com>
2024-03-31 12:34:55 +0700

    feat: add search button in navbar

--- weather-app/app/Components/Navbar.tsx
+++ weather-app/app/Components/Navbar.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <Button
+          className="source-code flex items-center gap-2"
+          onClick={() => {
+            router.push("https//github.com");
+          }}
+        >
+          Search
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
