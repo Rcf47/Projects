e491dd97acdd2600fb27959bf3c696e77ae75571
Rcf47 <xenos26@gmail.com>
2024-04-20 19:01:37 +0700

    feat: add ThemeDropdown component

--- weather-app/app/Components/Navbar.tsx
+++ weather-app/app/Components/Navbar.tsx
@@ -3,6 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { github } from "../utils/icons";
+import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
 
 function Navbar() {
   const router = useRouter();
@@ -10,13 +12,14 @@ function Navbar() {
     <div className="w-full py-4 flex items-center justify-between">
       <div className="left"></div>
       <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <ThemeDropdown />
         <Button
           className="source-code flex items-center gap-2"
           onClick={() => {
-            router.push("https//github.com");
+            router.push("https://github.com");
           }}
         >
-          Search
+          {github}  Source code
         </Button>
       </div>
     </div>
