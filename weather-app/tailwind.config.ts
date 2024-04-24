0e0306213d370bea17ba6567bfe0c6a7adcd935a
Rcf47 <xenos26@gmail.com>
2024-04-24 20:50:26 +0700

    feat: add new dark-grey in tailwind cond

--- weather-app/tailwind.config.ts
+++ weather-app/tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -24,6 +24,7 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        "dark-grey": "#A0A0A0",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
