d0fd6e35048e9f5062b7d86d0ff66445bd5afd8e
Rcf47 <xenos26@gmail.com>
2024-04-28 00:00:03 +0700

    feat: add styles for airpollution

--- weather-app/app/Components/AirPollution/AirPollution.tsx
+++ weather-app/app/Components/AirPollution/AirPollution.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useGlobalContext } from "@/app/context/globalContext";
 import { Skeleton } from "@/components/ui/skeleton";
 
@@ -11,9 +12,13 @@ function AirPollution() {
     !airQuality.list[0] ||
     !airQuality.list[0].main
   ) {
-    return <Skeleton className="h-[1rem] w-full col-span-2" />;
+    return <Skeleton className="h-[12rem] w-full col-span-2" />;
   }
-  return <div>AirPollution</div>;
+  return (
+    <div className="air-pollution col-span-full sm-2:col-span-2 pt-6 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none">
+      AirPollution
+    </div>
+  );
 }
 
 export default AirPollution;
