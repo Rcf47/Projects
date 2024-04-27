d51917c38613a0859a03e168125e8ec5ca225628
Rcf47 <xenos26@gmail.com>
2024-04-27 22:32:30 +0700

    feat: add new screen sizes in tailwind config

--- weather-app/tailwind.config.ts
+++ weather-app/tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -14,6 +14,8 @@ const config = {
       center: true,
       padding: "2rem",
       screens: {
+        "sm-2": "581px",
+        "md-2": "769px",
         "2xl": "1400px",
       },
     },
@@ -76,6 +78,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
