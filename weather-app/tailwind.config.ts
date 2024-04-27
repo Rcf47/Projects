890eac4f1fabed6a9533e0e74ea2de8a9510f85a
Rcf47 <xenos26@gmail.com>
2024-04-27 23:59:37 +0700

    fix: fix bug with wrong background

--- weather-app/tailwind.config.ts
+++ weather-app/tailwind.config.ts
@@ -26,7 +26,7 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        "dark-grey": "#A0A0A0",
+        "dark-grey": "#0A0A0A",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
