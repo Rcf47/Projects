1627627e9dcea8c7b75d0b6db24abd96e5ac6cc9
Rcf47 <xenos26@gmail.com>
2024-04-27 23:18:52 +0700

    feat: add air quality in react component

--- weather-app/app/Components/AirPollution/AirPollution.tsx
+++ weather-app/app/Components/AirPollution/AirPollution.tsx
@@ -1,4 +1,18 @@
+import { useGlobalContext } from "@/app/context/globalContext";
+import { Skeleton } from "@/components/ui/skeleton";
+
 function AirPollution() {
+  const { airQuality } = useGlobalContext();
+
+  // check if airQuality is available
+  if (
+    !airQuality ||
+    !airQuality.list ||
+    !airQuality.list[0] ||
+    !airQuality.list[0].main
+  ) {
+    return <Skeleton className="h-[1rem] w-full col-span-2" />;
+  }
   return <div>AirPollution</div>;
 }
 
