144455c79d8eb8e66240b57f7966d3732b4fa6f8
Rcf47 <xenos26@gmail.com>
2024-04-28 21:50:09 +0700

    feat: add AirPollution component

--- weather-app/app/Components/AirPollution/AirPollution.tsx
+++ weather-app/app/Components/AirPollution/AirPollution.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useGlobalContext } from "@/app/context/globalContext";
+import { thermo } from "@/app/utils/icons";
+import { airQualityText } from "@/app/utils/misc";
+import { Progress } from "@/components/ui/progress";
 import { Skeleton } from "@/components/ui/skeleton";
 
 function AirPollution() {
@@ -14,9 +17,19 @@ function AirPollution() {
   ) {
     return <Skeleton className="h-[12rem] w-full col-span-2" />;
   }
+  const airQualityIndex = airQuality.list[0].main.aqi * 10; // multiply by 10 to get percentage
+
+  const filteredText = airQualityText.find((item) => {
+    return item.rating === airQualityIndex;
+  });
+
   return (
     <div className="air-pollution col-span-full sm-2:col-span-2 pt-6 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none">
-      AirPollution
+      <h2 className="flex items-center gap-2 font-medium">
+        {thermo} Air pollution
+      </h2>
+      <Progress value={airQualityIndex} max={100} className="progress" />
+      <p>Air quality is {filteredText?.description}.</p>
     </div>
   );
 }
