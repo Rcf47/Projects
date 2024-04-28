25fb8556e631c0dbfb9a71456ea7c642de75f77c
Rcf47 <xenos26@gmail.com>
2024-04-28 22:28:20 +0700

    feat: add unixToTime func in misc

--- weather-app/app/utils/misc.tsx
+++ weather-app/app/utils/misc.tsx
@@ -1,7 +1,15 @@
+import moment from "moment";
+
 export const kelvinToCelcious = (kelvin: number) => {
   return Math.round(kelvin - 273.15);
 };
 
+export const unixToTime = (unix: number, timezone: number) => {
+  return moment
+    .unix(unix)
+    .utcOffset(timezone / 60)
+    .format("HH:mm");
+};
 export const airQualityText = [
   {
     rating: 20,
