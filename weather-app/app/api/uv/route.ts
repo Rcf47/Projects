587506e94920e9c7f55ca5dfe24d1c8252d62023
Rcf47 <xenos26@gmail.com>
2024-04-30 22:46:29 +0700

    feat: add new route in api uv

--- weather-app/app/api/uv/route.ts
+++ weather-app/app/api/uv/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
@@ -6,5 +6,13 @@ export async function GET(req: NextRequest) {
     const lon = 93.335;
 
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`;
-  } catch (error) {}
+    const res = await fetch(url, {
+      next: { revalidate: 900 },
+    });
+    const uvData = await res.json();
+    return NextResponse.json(uvData);
+  } catch (error) {
+    console.log("Error in getting uv data");
+    return new Response("Error getting uv data", { status: 500 });
+  }
 }
