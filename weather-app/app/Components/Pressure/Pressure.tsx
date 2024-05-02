7698ff091da887692b4f0247e72e5f8c0612e2a2
Rcf47 <xenos26@gmail.com>
2024-05-02 22:47:31 +0700

    refactor: if statement in pressure comp

--- weather-app/app/Components/Pressure/Pressure.tsx
+++ weather-app/app/Components/Pressure/Pressure.tsx
@@ -11,18 +11,25 @@ function Pressure() {
   }
   const { pressure } = forecast.main;
   const getPressureDescription = (pressure: number) => {
-    if (pressure < 1000) return "Very low pressure";
+    if (pressure < 1000) {
+      return "Very low pressure";
+    }
 
-    if (pressure >= 1000 && pressure < 1015)
+    if (pressure >= 1000 && pressure < 1015) {
       return "Low pressure. Expect weather changes.";
+    }
 
-    if (pressure >= 1015 && pressure < 1025)
+    if (pressure >= 1015 && pressure < 1025) {
       return "Normal pressure. Expect weather changes.";
+    }
 
-    if (pressure >= 1025 && pressure < 1040)
+    if (pressure >= 1025 && pressure < 1040) {
       return "High pressure. Expect weather changes.";
+    }
 
-    if (pressure >= 1040) return "Very high pressure. Expect weather changes.";
+    if (pressure >= 1040) {
+      return "Very high pressure. Expect weather changes.";
+    }
 
     return "Unavailable pressure data";
   };
