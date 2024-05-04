310960c6df49535701228eb0def6483cbb3d87dc
Rcf47 <xenos26@gmail.com>
2024-05-04 22:18:04 +0700

    feat: add lat and lon from searchparams in api

--- weather-app/app/api/fiveday/route.ts
+++ weather-app/app/api/fiveday/route.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-    const lat = 56.1199999;
-    const lon = 93.335;
+
+    const searchParams = req.nextUrl.searchParams;
+    const lat = searchParams.get("lat");
+    const lon = searchParams.get("lon");
+
     const dailyUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     const dailyRes = await fetch(dailyUrl, {
--- weather-app/app/api/weather/route.ts
+++ weather-app/app/api/weather/route.ts
@@ -4,8 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-    const lat = 56.1199999;
-    const lon = 93.335;
+
+    const searchParams = req.nextUrl.searchParams;
+    const lat = searchParams.get("lat");
+    const lon = searchParams.get("lon");
+
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const res = await axios.get(url);
     return NextResponse.json(res.data);
