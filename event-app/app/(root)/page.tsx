3fed8bc65285fc09a4b63cd89039432a0dc61c9b
Rcf47 <xenos26@gmail.com>
2024-05-09 09:58:56 +0700

    refactor: little refactor for main page

--- event-app/app/(root)/page.tsx
+++ event-app/app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Evently</h1>
-      <Button>Hello</Button>
+      <p>Hello world</p>
     </main>
   );
 }
