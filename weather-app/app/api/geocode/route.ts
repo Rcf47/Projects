3ab7b3f627b9155a8f6a5609053f1ae445181322
Vadim Cherepanov <xenos26@gmail.com>
2024-05-18 06:26:28 +0700

    feat: add changes in weather app for vercel deploy

--- weather-app/app/api/geocode/route.ts
+++ weather-app/app/api/geocode/route.ts
@@ -2,17 +2,17 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
   try {
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
-    const searchParams = req.nextUrl.searchParams;
     const city = searchParams.get("search");
 
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
     const res = await axios.get(url);
     return NextResponse.json(res.data);
   } catch (error) {
-    console.log("Error fetching geocode data: ", error.message);
+    console.log("Error fetching geocode data: ", (error as Error).message);
     return new Response("Error fetching geocode data", { status: 500 });
   }
 }
